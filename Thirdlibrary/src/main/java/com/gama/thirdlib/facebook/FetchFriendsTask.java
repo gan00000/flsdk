51f37ae9d842d1a571f8dba82f76a150ab254b1c
huangshaoguang <huangshaoguang@evatarchina.com>
2018-11-28 15:33:12 +0800

    好友分页请求

---
+++ Thirdlibrary/src/main/java/com/gama/thirdlib/facebook/FetchFriendsTask.java
@@ -0,0 +1,47 @@
+package com.gama.thirdlib.facebook;
+
+import android.content.Context;
+
+import com.core.base.bean.BaseReqeustBean;
+import com.core.base.request.AbsHttpRequest;
+import com.core.base.utils.PL;
+import com.core.base.utils.ToastUtils;
+
+public class FetchFriendsTask extends AbsHttpRequest {
+
+    private String url;
+    private Context context;
+
+    public FetchFriendsTask(Context context, String url) {
+        this.context = context;
+        this.url = url;
+
+        setGetMethod(true, false);
+    }
+
+
+    @Override
+    public BaseReqeustBean createRequestBean() {
+
+        BaseReqeustBean baseReqeustBean = new BaseReqeustBean();
+        baseReqeustBean.setCompleteUrl(url);
+
+        return baseReqeustBean;
+    }
+
+    @Override
+    public <T> void onHttpSucceess(T responseModel) {
+
+    }
+
+    @Override
+    public void onTimeout(String result) {
+        PL.i("onTimeout");
+        ToastUtils.toast(context, "connect timeout, please try again");
+    }
+
+    @Override
+    public void onNoData(String result) {
+        PL.i("onNoData");
+    }
+}
