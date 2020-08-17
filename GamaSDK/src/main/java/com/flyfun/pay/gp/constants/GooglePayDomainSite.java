757eb98d959fb5b8050b7f019539a0dc0e2a8ee9
ganyuanrong <372129081@qq.com>
2020-08-17 15:36:59 +0800

    修复充值域名错误
    服务条款提示不正确

--- GamaSDK/src/main/java/com/flyfun/pay/gp/constants/GooglePayDomainSite.java
+++ GamaSDK/src/main/java/com/flyfun/pay/gp/constants/GooglePayDomainSite.java
@@ -3,7 +3,7 @@ package com.flyfun.pay.gp.constants;
 public class GooglePayDomainSite {
 
 	public final static String GOOGLE_ORDER_CREATE = "api/dnmc-pay/v1/dynamic-creation.app";
-	public final static String GOOGLE_SEND = "api/apple-store-pay/v1/send-stone.app";
+	public final static String GOOGLE_SEND = "api/google-pay/v1/send-stone.app";
 
 	/**
 	 * .app的创单接口
@@ -13,6 +13,6 @@ public class GooglePayDomainSite {
 	/**
 	 * .app的发币接口
 	 */
-	public final static String APP_SURFIX_GOOGLE_SEND = "api/apple-store-pay/v1/send-stone.app";
+	public final static String APP_SURFIX_GOOGLE_SEND = "api/google-pay/v1/send-stone.app";
 
 }
