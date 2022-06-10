c8af44408cd485e97a43f8ec036c645c2d19fa44
Gan Yuanrong <372129081@qq.com>
2022-06-10 11:28:03 +0800

    绑定和欢迎页面账号不可点击

--- GamaSDK/src/main/java/com/mw/sdk/login/widget/v2/ThirdPlatBindAccountLayoutV2.java
+++ GamaSDK/src/main/java/com/mw/sdk/login/widget/v2/ThirdPlatBindAccountLayoutV2.java
@@ -103,17 +103,20 @@ public class ThirdPlatBindAccountLayoutV2 extends SLoginBaseRelativeLayout imple
         backView.setOnClickListener(this);
         bindConfirm.setOnClickListener(this);
 
+        GamaUtil.setAccountWithIcon(accountModel,sdkinputview_third_account.getIconImageView(),thirdAccountEditText);
+        pwdSdkInputEditTextView.setPwdInputEnable(false);
+
         return contentView;
     }
 
     @Override
     public void refreshViewData() {
         super.refreshViewData();
-        registerAccountEditText.setText("");
-        registerPasswordEditText.setText("");
-        thirdAccountEditText.setText("");
-
-        GamaUtil.setAccountWithIcon(accountModel,sdkinputview_third_account.getIconImageView(),thirdAccountEditText);
+//        registerAccountEditText.setText("");
+//        registerPasswordEditText.setText("");
+//        thirdAccountEditText.setText("");
+//
+//        GamaUtil.setAccountWithIcon(accountModel,sdkinputview_third_account.getIconImageView(),thirdAccountEditText);
 
     }
 
