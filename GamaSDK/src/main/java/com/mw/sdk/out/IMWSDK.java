ddb4d2bee7dc99a005d0de0588c71ed39ca1fa03
Gan Yuanrong <372129081@qq.com>
2022-07-07 18:27:42 +0800

    添加接口注释

--- GamaSDK/src/main/java/com/mw/sdk/out/IMWSDK.java
+++ GamaSDK/src/main/java/com/mw/sdk/out/IMWSDK.java
@@ -26,12 +26,37 @@ public interface IMWSDK extends IGameLifeCycle {
     void setGameLanguage(Activity activity,SGameLanguage gameLanguage);
 
     /**
-     * 保存角色信息
+     * 设置角色信息  在游戏获得角色信息的时候调用，每次登陆，切换账号等角色变化时调用
+     * @param roleId            角色id  				必传
+     * @param roleName          角色名   			必传
+     * @param roleLevel         角色等级			没有传空值 ""
+     * @param vipLevel          vip等级   			没有传空值 ""
+     * @param severCode         角色伺服器id 		必传
+     * @param serverName        角色伺服器名称	 	必传
      */
     void registerRoleInfo(Activity activity,String roleId,String roleName,String roleLevel,String vipLevel,String severCode,String serverName);
 
+    /**
+     * 登录
+     * @param activity
+     * @param iLoginCallBack        登录回调
+     */
     void login(Activity activity, ILoginCallBack iLoginCallBack);
 
+    /**
+     * @param activity
+     * @param payType           SPayType.WEB为平台网页第三方储值，SPayType.GOOGLE为Google储值
+     * @param cpOrderId         厂商订单号    必传
+     * @param productId         购买的商品id   必传
+     * @param extra             预留的穿透值   可选
+     * @param roleId            角色id  				必传
+     * @param roleName          角色名   			必传
+     * @param roleLevel         角色等级			没有传空值 ""
+     * @param vipLevel          vip等级   			没有传空值 ""
+     * @param severCode         角色伺服器id 		必传
+     * @param serverName        角色伺服器名称	 	必传
+     * @param listener          充值回调              辅助回调，充值是否成功以服务端回调为准
+     */
     void pay(Activity activity, SPayType payType, String cpOrderId, String productId, String extra, String roleId,String roleName,String roleLevel,String vipLevel,String severCode,String serverName, IPayListener listener);
 
 //    void openWebview(Activity activity);
