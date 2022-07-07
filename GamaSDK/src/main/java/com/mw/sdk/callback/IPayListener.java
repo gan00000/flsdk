90d1b93545b872ce2bce16cabb0622d0106d7dad
Gan Yuanrong <372129081@qq.com>
2022-07-07 18:34:18 +0800

    去掉payfinish

--- GamaSDK/src/main/java/com/mw/sdk/callback/IPayListener.java
+++ GamaSDK/src/main/java/com/mw/sdk/callback/IPayListener.java
@@ -3,7 +3,7 @@ package com.mw.sdk.callback;
 import android.os.Bundle;
 
 public interface IPayListener {
-    void onPayFinish(Bundle bundle);
+//    void onPayFinish(Bundle bundle);
     void onPaySuccess(String productId,String cpOrderId);
     void onPayFail();
 }
