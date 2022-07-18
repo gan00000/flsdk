0e1a7625d76721ce23b3ebd4beafeae9fe8647f4
Gan Yuanrong <372129081@qq.com>
2022-07-18 18:02:25 +0800

    修改WebActivity

--- GamaSDK/src/main/java/com/mw/sdk/SWebViewActivity.java
+++ GamaSDK/src/main/java/com/mw/sdk/SWebViewActivity.java
@@ -1,17 +1,14 @@
 package com.mw.sdk;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
-import com.core.base.BaseWebChromeClient;
-import com.core.base.BaseWebViewClient;
 import com.mw.base.widget.SWebView;
 import com.core.base.utils.PL;
-import com.core.base.utils.SStringUtil;
 import com.core.base.utils.ToastUtils;
 
 /**
@@ -22,16 +19,35 @@ public class SWebViewActivity extends SBaseSdkActivity {
 
     public static final String PLAT_WEBVIEW_URL = "PLAT_WEBVIEW_URL";
     public static final String PLAT_WEBVIEW_TITLE = "PLAT_WEBVIEW_TITLE";
-    private SWebView sWebView;
     private String webUrl;
     String webTitle;
     private ProgressBar progressBar;
+
+    private SWebViewLayout sWebViewLayout;
+    private SWebView sWebView;
+
+    public static Intent create(Activity activity, boolean title, boolean url){
+
+        Intent intent = new Intent(activity, SWebViewActivity.class);
+        intent.putExtra(PLAT_WEBVIEW_TITLE,title);
+        intent.putExtra(PLAT_WEBVIEW_URL,url);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.s_web_view_with_title_layout);
+        setContentView(R.layout.s_web_act_layout);
+        sWebViewLayout = findViewById(R.id.sl_web_act);
+        sWebView = sWebViewLayout.getsWebView();
+
+        sWebViewLayout.getCloseImageView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         if (getIntent() != null) {
             webUrl = getIntent().getStringExtra(PLAT_WEBVIEW_URL);
@@ -39,22 +55,12 @@ public class SWebViewActivity extends SBaseSdkActivity {
 //            initTitle(webTitle);
         }
 
-
         if (TextUtils.isEmpty(webUrl)){
             ToastUtils.toast(getApplicationContext(),"url error");
             PL.i("webUrl is empty");
-            finish();
-            return;
+        }else{
+            sWebView.loadUrl(webUrl);
         }
-
-
-        progressBar = (ProgressBar) findViewById(R.id.s_webview_pager_loading_percent);
-        sWebView = (SWebView) findViewById(R.id.s_webview_id);
-
-        sWebView.setBaseWebChromeClient(new BaseWebChromeClient(progressBar,this));
-        sWebView.setWebViewClient(new BaseWebViewClient(this));
-        PL.i("SWebViewActivity url:" + webUrl);
-        sWebView.loadUrl(webUrl);
 
     }
 
@@ -71,8 +77,11 @@ public class SWebViewActivity extends SBaseSdkActivity {
     protected void onDestroy() {
         super.onDestroy();
         if (sWebView != null){
+            sWebView.clearCache(true);
+            sWebView.clearHistory();
             sWebView.destroy();
             sWebView = null;
+            PL.i("dialog destory webview");
         }
     }
 }
