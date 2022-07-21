f3e73e3c88ce6b00ae71308585436cfc3d06c8b6
Gan Yuanrong <372129081@qq.com>
2022-07-21 14:49:41 +0800

    添加分享demo

--- GamaSdkDemo/src/main/java/com/game/sdk/demo/MainActivity.java
+++ GamaSdkDemo/src/main/java/com/game/sdk/demo/MainActivity.java
@@ -274,6 +274,22 @@ public class MainActivity extends Activity {
                 });
             }
         });
+        findViewById(R.id.demo_share2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mIMWSDK.share(MainActivity.this, "#萬靈召喚師","2022首款卡牌大作【萬靈召喚師】，爆笑來襲！從東方文明到西方文明的羈絆，從神族到魔族的對抗，一段奇妙的神仙冒險之旅就此展開！","https://static-resource.meowplayer.com/share/index.html", new ISdkCallBack() {
+                    @Override
+                    public void success() {
+                        PL.i("share success");
+                    }
+
+                    @Override
+                    public void failure() {
+                        PL.i("share failure");
+                    }
+                });
+            }
+        });
     }
 
     @Override
