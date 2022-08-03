7282b05e5ac774183738d97f0c30b5fb619fba5e
Gan Yuanrong <372129081@qq.com>
2022-08-03 16:11:21 +0800

    修改绑定手机

--- GamaSDK/src/sdk2/java/com/mw/sdk/login/widget/v2/AccountBindPhoneLayout.java
+++ GamaSDK/src/sdk2/java/com/mw/sdk/login/widget/v2/AccountBindPhoneLayout.java
@@ -1,5 +1,6 @@
 package com.mw.sdk.login.widget.v2;
 
+import android.app.Activity;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -9,8 +10,15 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.core.base.callback.SFCallBack;
+import com.core.base.utils.SStringUtil;
+import com.core.base.utils.ToastUtils;
+import com.mw.base.bean.PhoneInfo;
 import com.mw.sdk.R;
+import com.mw.sdk.api.Request;
+import com.mw.sdk.login.PhoneAreaCodeDialogHelper;
 import com.mw.sdk.login.widget.SLoginBaseRelativeLayout;
+import com.mw.sdk.out.ISdkCallBack;
 
 /**
  * Created by GanYuanrong on 2017/2/6.
@@ -35,6 +43,8 @@ public class AccountBindPhoneLayout extends SLoginBaseRelativeLayout {
     private View ll_has_bind_view;
     private TextView tv_area_code_2;
     private EditText et_input_phone_number_2;
+
+    private PhoneAreaCodeDialogHelper phoneAreaCodeDialogHelper;
 
     public AccountBindPhoneLayout(Context context) {
         super(context);
@@ -78,7 +88,9 @@ public class AccountBindPhoneLayout extends SLoginBaseRelativeLayout {
         iv_bind_phone_close.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if (sBaseDialog != null) {
+                    sBaseDialog.dismiss();
+                }
             }
         });
 
@@ -87,19 +99,79 @@ public class AccountBindPhoneLayout extends SLoginBaseRelativeLayout {
             @Override
             public void onClick(View v) {
                 //弹出区号选择
-
+                if (phoneAreaCodeDialogHelper == null){
+                    phoneAreaCodeDialogHelper = new PhoneAreaCodeDialogHelper((Activity) getContext());
+                }
+                phoneAreaCodeDialogHelper.showPhoneAreaCodeDialog(new PhoneAreaCodeDialogHelper.AreaCodeSelectCallback() {
+                    @Override
+                    public void select(PhoneInfo phoneInfo) {
+                        tv_area_code.setText(phoneInfo.getValue());
+                    }
+                });
             }
         });
         btn_find_get_vfcode.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 //请求发送验证码
+                String areaCode = tv_area_code.getText().toString();
+                String phone = et_input_phone_number.getEditableText().toString();
+                String vfCode = et_input_vf_code.getEditableText().toString();
+
+                if (SStringUtil.isEmpty(areaCode)) {
+                    ToastUtils.toast(getContext(),R.string.text_area_code_not_empty);
+                    return;
+                }
+                if (SStringUtil.isEmpty(phone)) {
+                    ToastUtils.toast(getContext(),R.string.text_phone_not_empty);
+                    return;
+                }
+                Request.sendVfCode(getContext(),true, areaCode, phone, new ISdkCallBack() {
+                    @Override
+                    public void success() {
+
+                    }
+
+                    @Override
+                    public void failure() {
+
+                    }
+                });
             }
         });
 
         okBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String areaCode = tv_area_code.getText().toString();
+                String phone = et_input_phone_number.getEditableText().toString();
+                String vfCode = et_input_vf_code.getEditableText().toString();
+
+                if (SStringUtil.isEmpty(areaCode)) {
+                    ToastUtils.toast(getContext(),R.string.text_area_code_not_empty);
+                    return;
+                }
+                if (SStringUtil.isEmpty(phone)) {
+                    ToastUtils.toast(getContext(),R.string.text_phone_not_empty);
+                    return;
+                }
+                if (SStringUtil.isEmpty(vfCode)) {
+                    ToastUtils.toast(getContext(),R.string.py_vfcode_empty);
+                    return;
+                }
+
+                Request.bindPhone(getContext(),true, areaCode, phone, vfCode, new SFCallBack<String>() {
+                    @Override
+                    public void success(String result, String msg) {
+
+                    }
+
+                    @Override
+                    public void fail(String result, String msg) {
+
+                    }
+                });
 
             }
         });
@@ -131,6 +203,7 @@ public class AccountBindPhoneLayout extends SLoginBaseRelativeLayout {
     public void setVisibility(int visibility) {
         super.setVisibility(visibility);
     }
+
 
 
 }
