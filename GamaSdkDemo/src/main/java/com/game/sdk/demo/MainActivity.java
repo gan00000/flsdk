c690425e6fd06c23002ea1b3ae018635475741eb
Gan Yuanrong <372129081@qq.com>
2023-01-05 16:08:00 +0800

    添加推送测试代码，icon

--- GamaSdkDemo/src/main/java/com/game/sdk/demo/MainActivity.java
+++ GamaSdkDemo/src/main/java/com/game/sdk/demo/MainActivity.java
@@ -4,9 +4,16 @@ import android.Manifest;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -17,6 +24,7 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
 import com.core.base.bean.BaseResponseModel;
 import com.core.base.callback.SFCallBack;
@@ -53,7 +61,7 @@ public class MainActivity extends Activity {
     String roleName = "貪婪聖殿"; //角色名
     String roleLevel = "106"; //角色等级
     String vipLevel = "5"; //角色vip等级
-    String serverCode = "999"; //角色伺服器id
+    String serverCode = "1"; //角色伺服器id
     String serverName = "S1"; //角色伺服器名称
 
     @Override
@@ -189,7 +197,7 @@ public class MainActivity extends Activity {
 
 //                com.game.superand.1usd
 //                com.game.superand.2usd
-                String skuId = "com.wanye.fszhl.lb1";
+                String skuId = "com_xinhai_chmxt_3card";
                 mIMWSDK.pay(MainActivity.this, SPayType.GOOGLE, "" + System.currentTimeMillis(),skuId, "xxxx","role_id_1","role_name","role_level","vipLevel",serverCode, serverName, new IPayListener() {
 
                     @Override
@@ -233,7 +241,7 @@ public class MainActivity extends Activity {
         webPayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String skuId = "com.mplay.wlzhs.lb2";
+                String skuId = "com_xinhai_chmxt_1usd";
                 mIMWSDK.pay(MainActivity.this, SPayType.WEB, "" + System.currentTimeMillis(),skuId, "xxxx", "role_id_1","role_name","role_level","vipLevel","1001", serverName, new IPayListener() {
 
                     @Override
@@ -527,5 +535,118 @@ public class MainActivity extends Activity {
 ////                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
 //            }
 //        }
+    }
+
+
+    private void sendNotification(String messageTitle, String messageBody) {
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if(notificationManager == null) {
+            return;
+        }
+
+        PendingIntent pendingIntent = null;
+        String pkName = getApplicationContext().getPackageName();
+        if (!TextUtils.isEmpty(pkName)) {
+            Intent intent = getApplicationContext().getPackageManager().getLaunchIntentForPackage(pkName);
+            if(intent != null) {
+
+// Targeting S+ (version 31 and above) requires that one of FLAG_IMMUTABLE or FLAG_MUTABLE be specified when creating a PendingIntent.
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+//                pendingIntent = PendingIntent.getActivity(this, 0, intent,
+//                        PendingIntent.FLAG_ONE_SHOT);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    pendingIntent = PendingIntent.getActivity(this,
+                            0, intent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
+
+                }else {
+                    pendingIntent = PendingIntent.getActivity(this,
+                            0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+                }
+            }
+        }
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        int icon = getPushIconId(getApplicationContext());
+        if(TextUtils.isEmpty(messageTitle)) {
+            messageTitle = getApplicationName(this);
+        }
+
+//        小图标：必须提供，通过 setSmallIcon() 进行设置。
+//        应用名称：由系统提供。
+//        时间戳：由系统提供，但您可以使用 setWhen() 替换它或者使用 setShowWhen(false) 隐藏它。
+//        大图标：可选内容（通常仅用于联系人照片，请勿将其用于应用图标），通过 setLargeIcon() 进行设置。
+//        标题：可选内容，通过 setContentTitle() 进行设置。
+//        文本：可选内容，通过 setContentText() 进行设置
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setContentTitle(messageTitle)
+                .setSmallIcon(icon)
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri);
+
+        if (pendingIntent != null) {
+            notificationBuilder.setContentIntent(pendingIntent);
+        }
+
+        if (Build.VERSION.SDK_INT >= 26) {
+
+            // The id of the channel.
+            String id = "nw_notify_channel";
+            // The user-visible name of the channel.
+//            String name = context.getApplicationInfo().name;
+            String name = "new message";
+            //范围是 IMPORTANCE_NONE(0) 至 IMPORTANCE_HIGH(4)。默认重要性级别为 3：在所有位置显示，发出提示音，但不会对用户产生视觉干扰。
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel mChannel = new NotificationChannel(id, name, importance);
+            // Configure the notification channel.
+            mChannel.enableLights(true);
+            // Sets the notification light color for notifications posted to this
+            // channel, if the device supports this feature.
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            notificationManager.createNotificationChannel(mChannel);
+            notificationBuilder.setChannelId(id);
+        }
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
+    public static String getApplicationName(Context context) {
+        ApplicationInfo applicationInfo = context.getApplicationInfo();
+        int stringId = applicationInfo.labelRes;
+        return stringId == 0 ? applicationInfo.nonLocalizedLabel.toString() : context.getString(stringId);
+    }
+
+//     * 获取推送icon的id
+//     * @return 先读取meta-data配置(com.google.firebase.messaging.default_notification_icon)，然后读取应用icon。
+
+
+    public static int getPushIconId(Context context) {
+        int iconId = 0;
+        try {
+            ApplicationInfo appInfoappInfo = context.getPackageManager().getApplicationInfo(context.getPackageName(),
+                    PackageManager.GET_META_DATA);
+            Bundle metaData = appInfoappInfo.metaData;
+            if(metaData != null) {
+                iconId = metaData.getInt("com.google.firebase.messaging.default_notification_icon");
+            } else {
+                PL.i("当前应用没有在meta-data配置");
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if(iconId == 0) {
+            ApplicationInfo info = context.getApplicationInfo();
+            iconId = info.icon;
+            PL.i("没有找到推送icon，使用应用icon : " + iconId);
+        }
+        return iconId;
     }
 }
