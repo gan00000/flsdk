6d9b816071ca5a81439f42f4c41cc1efc2f9722a
Gan Yuanrong <372129081@qq.com>
2023-02-23 10:42:10 +0800

    删掉public class GamaBroadcastReceiver

--- GamaSDK/src/main/java/com/mw/sdk/GamaBroadcastReceiver.java
+++ GamaSDK/src/main/java/com/mw/sdk/GamaBroadcastReceiver.java
@@ -12,19 +12,19 @@ import com.mw.base.utils.SdkUtil;
  * Created by gan on 2017/8/29.
  */
 
-public class GamaBroadcastReceiver extends BroadcastReceiver {
-
-    public static final String INSTALL_REFERRER_ACTION = "com.android.vending.INSTALL_REFERRER";
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        PL.i("GamaBroadcastReceiver onReceive");
-        if (intent != null && INSTALL_REFERRER_ACTION.equals(intent.getAction())){
-            String referrer = intent.getStringExtra("referrer");
-            PL.i("referrer:" + referrer);
-            if (SStringUtil.isNotEmpty(referrer)){
-                SdkUtil.saveReferrer(context,referrer);
-            }
-        }
-    }
-}
+//public class GamaBroadcastReceiver extends BroadcastReceiver {
+//
+//    public static final String INSTALL_REFERRER_ACTION = "com.android.vending.INSTALL_REFERRER";
+//
+//    @Override
+//    public void onReceive(Context context, Intent intent) {
+//        PL.i("GamaBroadcastReceiver onReceive");
+//        if (intent != null && INSTALL_REFERRER_ACTION.equals(intent.getAction())){
+//            String referrer = intent.getStringExtra("referrer");
+//            PL.i("referrer:" + referrer);
+//            if (SStringUtil.isNotEmpty(referrer)){
+//                SdkUtil.saveReferrer(context,referrer);
+//            }
+//        }
+//    }
+//}
