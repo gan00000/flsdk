757d99820959c0696ab41d65753a93ad1c0657bc
Gan Yuanrong <372129081@qq.com>
2023-03-07 16:00:27 +0800

    华为屏蔽分享

--- GamaSDK/src/huawei/java/com/mw/sdk/out/MWSdkImpl.java
+++ GamaSDK/src/huawei/java/com/mw/sdk/out/MWSdkImpl.java
@@ -1,5 +1,21 @@
 package com.mw.sdk.out;
 
+import android.app.Activity;
+
 public class MWSdkImpl extends BaseSdkImpl {
 
+    @Override
+    public void share(Activity activity, String hashTag, String message, String shareLinkUrl, ISdkCallBack iSdkCallBack) {
+
+    }
+
+    @Override
+    public void shareFacebook(Activity activity, String hashTag, String message, String shareLinkUrl, ISdkCallBack iSdkCallBack) {
+
+    }
+
+    @Override
+    public void shareLine(Activity activity, String content, ISdkCallBack iSdkCallBack) {
+
+    }
 }
