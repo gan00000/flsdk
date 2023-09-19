1d1cdc7e089db6b297f83be81768287f29239547
Gan Yuanrong <372129081@qq.com>
2023-09-19 11:45:46 +0800

    添加js事件追踪

--- GamaSDK/src/main/java/com/core/base/js/WebViewJsObj.java
+++ GamaSDK/src/main/java/com/core/base/js/WebViewJsObj.java
@@ -5,7 +5,13 @@ import android.app.Activity;
 import android.webkit.JavascriptInterface;
 
 import com.core.base.utils.PL;
+import com.core.base.utils.SStringUtil;
+import com.mw.sdk.ads.EventConstant;
+import com.mw.sdk.ads.SdkEventLogger;
 import com.mw.sdk.pay.MWWebPayActivity;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class WebViewJsObj {
 
@@ -18,8 +24,28 @@ public class WebViewJsObj {
     @SuppressLint("JavascriptInterface")
     @JavascriptInterface
     public void close(){
+        PL.i("js close");
         if (activity != null){
             activity.finish();
+        }
+    }
+
+    @SuppressLint("JavascriptInterface")
+    @JavascriptInterface
+    public void trackEvent(String eventName){
+        PL.i("js trackEvent:" + eventName);
+        if (activity != null && SStringUtil.isNotEmpty(eventName)){
+//            if (SStringUtil.isNotEmpty(dataJson)){
+//
+//            }
+
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    SdkEventLogger.sendEventToSever(activity, eventName);
+                    SdkEventLogger.trackingWithEventName(activity, eventName, null, EventConstant.AdType.AdTypeAllChannel);
+                }
+            });
         }
     }
 }
