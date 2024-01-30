e8d6f1824c4a7fc0f1fcfc781a9dcec249dc1196
Gan Yuanrong <372129081@qq.com>
2024-01-30 17:53:11 +0800

    修改demo

--- SdkDemo/src/main/java/com/game/sdk/demo/MainActivity.java
+++ SdkDemo/src/main/java/com/game/sdk/demo/MainActivity.java
@@ -389,8 +389,6 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
 
-                sendNotification("tests觉得基督教", "kdkkdk好的哈哈哈哈");
-
                 Date ydate = TimeUtil.getYesterday(Long.parseLong(tempSLoginResponse.getData().getTimestamp()));
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                 PL.i(sdf.format(ydate));
@@ -558,115 +556,5 @@ public class MainActivity extends Activity {
     }
 
 
-    private void sendNotification(String messageTitle, String messageBody) {
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        if(notificationManager == null) {
-            return;
-        }
-
-        PendingIntent pendingIntent = null;
-        String pkName = getApplicationContext().getPackageName();
-        if (!TextUtils.isEmpty(pkName)) {
-            Intent intent = getApplicationContext().getPackageManager().getLaunchIntentForPackage(pkName);
-            if(intent != null) {
-
-// Targeting S+ (version 31 and above) requires that one of FLAG_IMMUTABLE or FLAG_MUTABLE be specified when creating a PendingIntent.
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-//                pendingIntent = PendingIntent.getActivity(this, 0, intent,
-//                        PendingIntent.FLAG_ONE_SHOT);
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                    pendingIntent = PendingIntent.getActivity(this,
-                            0, intent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
-
-                }else {
-                    pendingIntent = PendingIntent.getActivity(this,
-                            0, intent, PendingIntent.FLAG_ONE_SHOT);
-
-                }
-            }
-        }
-
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-
-        int icon = getPushIconId(getApplicationContext());
-        if(TextUtils.isEmpty(messageTitle)) {
-            messageTitle = getApplicationName(this);
-        }
-
-//        小图标：必须提供，通过 setSmallIcon() 进行设置。
-//        应用名称：由系统提供。
-//        时间戳：由系统提供，但您可以使用 setWhen() 替换它或者使用 setShowWhen(false) 隐藏它。
-//        大图标：可选内容（通常仅用于联系人照片，请勿将其用于应用图标），通过 setLargeIcon() 进行设置。
-//        标题：可选内容，通过 setContentTitle() 进行设置。
-//        文本：可选内容，通过 setContentText() 进行设置
-
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setContentTitle(messageTitle)
-                .setSmallIcon(icon)
-                .setContentText(messageBody)
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri);
-
-        if (pendingIntent != null) {
-            notificationBuilder.setContentIntent(pendingIntent);
-        }
-
-        if (Build.VERSION.SDK_INT >= 26) {
-
-            // The id of the channel.
-            String id = "nw_notify_channel";
-            // The user-visible name of the channel.
-//            String name = context.getApplicationInfo().name;
-            String name = "new message";
-            //范围是 IMPORTANCE_NONE(0) 至 IMPORTANCE_HIGH(4)。默认重要性级别为 3：在所有位置显示，发出提示音，但不会对用户产生视觉干扰。
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel mChannel = new NotificationChannel(id, name, importance);
-            // Configure the notification channel.
-            mChannel.enableLights(true);
-            // Sets the notification light color for notifications posted to this
-            // channel, if the device supports this feature.
-            mChannel.setLightColor(Color.RED);
-            mChannel.enableVibration(true);
-            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
-            notificationManager.createNotificationChannel(mChannel);
-            notificationBuilder.setChannelId(id);
-        }
-
-        notificationManager.notify(0, notificationBuilder.build());
-    }
-
-    public static String getApplicationName(Context context) {
-        ApplicationInfo applicationInfo = context.getApplicationInfo();
-        int stringId = applicationInfo.labelRes;
-        return stringId == 0 ? applicationInfo.nonLocalizedLabel.toString() : context.getString(stringId);
-    }
-
-//     * 获取推送icon的id
-//     * @return 先读取meta-data配置(com.google.firebase.messaging.default_notification_icon)，然后读取应用icon。
-
-
-    public static int getPushIconId(Context context) {
-        int iconId = 0;
-        try {
-            ApplicationInfo appInfoappInfo = context.getPackageManager().getApplicationInfo(context.getPackageName(),
-                    PackageManager.GET_META_DATA);
-            Bundle metaData = appInfoappInfo.metaData;
-            if(metaData != null) {
-                iconId = metaData.getInt("com.google.firebase.messaging.default_notification_icon");
-            } else {
-                PL.i("当前应用没有在meta-data配置");
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        if(iconId == 0) {
-            ApplicationInfo info = context.getApplicationInfo();
-            iconId = info.icon;
-            PL.i("没有找到推送icon，使用应用icon : " + iconId);
-        }
-        return iconId;
-    }
 }
