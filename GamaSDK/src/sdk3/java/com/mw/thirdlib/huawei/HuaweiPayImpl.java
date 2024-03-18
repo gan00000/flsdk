cde5079dc46402fa82b40a2c99a24de678db2a50
Gan Yuanrong <372129081@qq.com>
2024-03-18 17:36:52 +0800

    添加无用

---
+++ GamaSDK/src/sdk3/java/com/mw/thirdlib/huawei/HuaweiPayImpl.java
@@ -0,0 +1,38 @@
+package com.mw.thirdlib.huawei;
+
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.mw.sdk.bean.req.PayCreateOrderReqBean;
+import com.mw.sdk.pay.IPayCallBack;
+
+public class HuaweiPayImpl {
+
+    public static final int HW_IAP_REQUEST_CODE_ENVREADY = 6660;
+    public static final int HW_IAP_REQUEST_CODE_PURCHASE = 6661;
+
+    private Activity mActivity;
+
+    private String productId;
+    private String cpOrderId;
+    private String extra;
+
+
+    public void setPayCallBack(IPayCallBack iPayCallBack) {
+    }
+
+    public HuaweiPayImpl(Activity mActivity) {
+        this.mActivity = mActivity;
+    }
+
+
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+
+    }
+
+    public void startPay(Activity activity, PayCreateOrderReqBean createOrderIdReqBean) {
+
+    }
+
+}
---
+++ GamaSDK/src/sdk3/java/com/mw/thirdlib/huawei/HuaweiSignIn.java
@@ -0,0 +1,49 @@
+package com.mw.thirdlib.huawei;
+
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+public class HuaweiSignIn {
+
+
+	HWSignInCallBack signInCallBack;
+
+	public HuaweiSignIn(Activity activity) {
+	}
+
+	public HuaweiSignIn(Activity activity, Dialog dialog) {
+
+	}
+
+	public HuaweiSignIn(FragmentActivity fragmentActivity, Fragment fragment, Dialog dialog) {
+
+	}
+
+
+	public void startSignIn(Activity activity, HWSignInCallBack mSignInCallBack){
+	}
+
+	public void signOut(Activity activity) {
+
+	}
+	public void handleActivityResult(Context context, int requestCode, int resultCode, Intent data) {
+
+	}
+
+
+
+//	============firebase 1=================
+
+
+	public interface HWSignInCallBack{
+		void success(String id, String mFullName, String mEmail, String idTokenString);
+		void failure();
+	}
+
+}
