bcc67db7a9e44347b53e45e5c2a409704b15ee5c
Gan Yuanrong <372129081@qq.com>
2024-09-24 18:16:04 +0800

    优化华为渠道SDK

--- GamaSDK/src/huawei/java/com/mw/sdk/out/MWSdkImpl.java
+++ GamaSDK/src/huawei/java/com/mw/sdk/out/MWSdkImpl.java
@@ -8,22 +8,30 @@ public class MWSdkImpl extends BaseSdkImpl {
 
     @Override
     public void share(Activity activity, String hashTag, String message, String shareLinkUrl, ISdkCallBack iSdkCallBack) {
-
+        if (iSdkCallBack != null){
+            iSdkCallBack.success();
+        }
     }
 
     @Override
     public void shareFacebook(Activity activity, String hashTag, String message, String shareLinkUrl, ISdkCallBack iSdkCallBack) {
-
+        if (iSdkCallBack != null){
+            iSdkCallBack.success();
+        }
     }
 
     @Override
     public void shareLine(Activity activity, String content, ISdkCallBack iSdkCallBack) {
-
+        if (iSdkCallBack != null){
+            iSdkCallBack.success();
+        }
     }
 
     @Override
     public void requestStoreReview(Activity activity, SFCallBack sfCallBack) {
-
+        if (sfCallBack != null){
+            sfCallBack.success(null,"");
+        }
     }
 
     @Override
