7367fa3bc9370171adf5e3f5e88368425de54114
Gan Yuanrong <372129081@qq.com>
2025-06-03 16:45:44 +0800

    demo修改

--- GamaSdkDemo/src/main/java/com/game/sdk/demo/MainActivity.java
+++ GamaSdkDemo/src/main/java/com/game/sdk/demo/MainActivity.java
@@ -121,7 +121,8 @@ public class MainActivity extends Activity {
 //                com.game.superand.1usd
 //                com.game.superand.2usd
 //                String skuId = "com.miaoou.6jin";
-                String skuId = "com.msg.en099";
+//                String skuId = "com.msg.en099";
+                String skuId = "com.wmwx.sgcz.99";
                 mIMWSDK.pay(MainActivity.this, SPayType.GOOGLE, "" + System.currentTimeMillis(),skuId, extra,roleId,roleName,roleLevel, vipLevel,serverCode, serverName, new IPayListener() {
 
                     @Override
