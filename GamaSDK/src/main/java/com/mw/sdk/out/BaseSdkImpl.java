a03901836b2f5c0799779bcf64048026deade8e5
Gan Yuanrong <372129081@qq.com>
2025-09-18 15:17:12 +0800

    修复支付方式跳转

--- GamaSDK/src/main/java/com/mw/sdk/out/BaseSdkImpl.java
+++ GamaSDK/src/main/java/com/mw/sdk/out/BaseSdkImpl.java
@@ -1029,7 +1029,12 @@ public class BaseSdkImpl implements IMWSDK {
         } else if(ChannelPlatform.LUNQI.getChannel_platform().equals(channel_platform)) {
 //            doLunqiPay(activity, payCreateOrderReqBean);
             doWebPay(activity, payCreateOrderReqBean);
-        }else if (ChannelPlatform.GOOGLE.getChannel_platform().equals(channel_platform)){
+        }else if (ChannelPlatform.GOOGLE.getChannel_platform().equals(channel_platform)
+                || ChannelPlatform.VK.getChannel_platform().equals(channel_platform)
+                || ChannelPlatform.ONESTORE.getChannel_platform().equals(channel_platform)
+                || ChannelPlatform.SAMSUNG.getChannel_platform().equals(channel_platform)
+                || ChannelPlatform.Xiaomi.getChannel_platform().equals(channel_platform)
+                || ChannelPlatform.NOWGG.getChannel_platform().equals(channel_platform)){
             checkGoogleOrWebPay(activity, payCreateOrderReqBean);
         }else {
             doWebPay(activity, payCreateOrderReqBean);
